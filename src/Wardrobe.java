247626985a516bd2d2725196ddec19a74c33b706
yaboi14 <mateotmedak08@gmail.com>
2026-04-13 10:01:53 -0700

    Added more classes, and started working on Wardrobe

--- src/Wardrobe.java
+++ src/Wardrobe.java
@@ -1,3 +1,16 @@
-public class Wardrobe {
 
+import java.util.ArrayList;
+
+public class Wardrobe {
+    ArrayList Wardrobe = new ArrayList();
+
+    public void addItem(ClothingItem clothingItem){
+        Wardrobe.add(clothingItem);
+    }
+
+    public void seeWardrobe(){
+        for(int i = 0; i <= Wardrobe.length; i++ ){
+
+        }
+    }
 }
