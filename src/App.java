424b948a11b9d541d8d9a5e06c8566bb9383ab7a
yaboi14 <mateotmedak08@gmail.com>
2026-04-29 09:38:53 -0700

    Worked on user interface

--- src/App.java
+++ src/App.java
@@ -11,8 +11,20 @@ public class App {
             System.out.println("Add new clothing item to wardrobe!");
             System.out.println("Item Type (Hat,Shirt,Pants, or Shoes):");
             String type = scan.next();
+
+            while (true) {
+                if(!(type.equalsIgnoreCase("hat") || type.equalsIgnoreCase("shirt") || type.equalsIgnoreCase("pants") || type.equalsIgnoreCase("shoes"))){
+                System.out.println("Please give valid answer, choose between Hat, Shirt, Pants, or Shoes " );
+                type = scan.next();
+                }
+                else{
+                    break;
+                }
+            }
+
             System.out.println("Clothing Color: ");
             String color = scan.next();
+
             new ClothingItem(type,color,wardrobe);
             System.out.println("");
             System.out.println("Add more clothing?");
@@ -25,20 +37,47 @@ public class App {
             }
         }
 
-
-        // Create manager
         OutfitManager manager = new OutfitManager();
 
-        // Generate and save multiple outfits
-        for(int i = 0; i < 5; i++){
-            Outfit outfit = OutfitGenerator.generateOutfit(wardrobe);
+    while (true) {
 
-            if(outfit != null){
-                manager.saveOutfit(outfit);
-            }
+        System.out.println("\nWould you like to generate an outfit? (yes/no)");
+        String answer = scan.next();
+
+        if(answer.equalsIgnoreCase("no")){
+            break;
         }
 
-        // Display saved outfits
-        manager.displayOutfits();
+        if(answer.equalsIgnoreCase("yes")){
+
+            Outfit fit1 = OutfitGenerator.generateOutfit(wardrobe);
+
+            if(fit1 != null){
+                System.out.println("\nGenerated Outfit:");
+                System.out.println(fit1);
+
+            // SAVE?
+                while (true) {
+                    System.out.println("Would you like to save the outfit? (yes/no)");
+                    String answer2 = scan.next();
+
+                    if(answer2.equalsIgnoreCase("yes")){
+                        manager.saveOutfit(fit1);
+                        System.out.println("Outfit saved!");
+                        break;
+                    }
+                    else if(answer2.equalsIgnoreCase("no")){
+                        break;
+                    }
+                    else{
+                        System.out.println("Invalid input.");
+                    }
+                }
+            }
+        }
+        else{
+            System.out.println("Please enter yes or no.");
+            }
+        }
     }
 }
